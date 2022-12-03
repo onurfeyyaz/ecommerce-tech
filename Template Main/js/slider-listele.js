6e2bc5743b7d88b2d9693ee0f6ed05aa6565639e
onurfeyyaz <feyyazonur0@gmail.com>
2022-12-03 23:07:46 +0300

    add urunler

--- Template Main/js/slider-listele.js
+++ Template Main/js/slider-listele.js
@@ -2,50 +2,63 @@ const sliderArr = [
     {
         'title':'Ürün İsmi',
         'price':'Ürün Fiyatı',
+        'detail':"Ürün detayı",
+        'category':"televizyon",
+        'seller':"satıcısı",
         'src':"https://picsum.photos/200",
     },
     {
         'title':'Ürün İsmi',
         'price':'Ürün Fiyatı',
-          'src':"https://picsum.photos/200",
+        'detail':"Ürün detayı",
+        'category':"televizyon",
+        'seller':"satıcısı",
+        'src':"https://picsum.photos/200",
     },
     {
         'title':'Ürün İsmi',
         'price':'Ürün Fiyatı',
-          'src':"https://picsum.photos/200",
+        'detail':"Ürün detayı",
+        'category':"televizyon",
+        'seller':"satıcısı",
+        'src':"https://picsum.photos/200",
     },
     {
         'title':'Ürün İsmi',
         'price':'Ürün Fiyatı',
-          'src':"https://picsum.photos/200",
+        'detail':"Ürün detayı",
+        'category':"televizyon",
+        'seller':"satıcısı",
+        'src':"https://picsum.photos/200",
     },
 ]
 
 
-
-const singleProduct = document.querySelectorAll('.single-product')
 const titleProduct = document.querySelectorAll('.urunTitle')
 const priceProduct = document.querySelectorAll('.product-price')
 
 // product detail
-
-const productDetailTitle = document.querySelector('.product-name')
+const productDetailImage = document.querySelector('.product-detail-images')
+const productDetailTitle = document.querySelector('.product-detail-name')
+const productDetailSeller = document.querySelector('.product-detail-seller')
+const productDetailFeatures = document.querySelector('.product-detail-features')
+const productDetailPrice = document.querySelector('.product-detail-price')
 
 let seciliData = {}
-const seciliUrun = (data)=> {
-    
-    console.log('urunTitle',titleProduct[data].textContent)
-    console.log('urunPrice',priceProduct[data].textContent.trim())
+const seciliUrun = (data) => {
     localStorage.removeItem('urunTitle')
     localStorage.removeItem('urunPrice')
+
     localStorage.setItem('urunTitle',titleProduct[data].textContent)
     localStorage.setItem('urunPrice',priceProduct[data].textContent.trim())
 }
 
 window.addEventListener('DOMContentLoaded',function (){
     console.log("this.localStorage.getItem('title')",this.localStorage.getItem('urunTitle'))
-    if(this.localStorage.getItem('urunTitle')){
+
+    if(this.localStorage.getItem('urunTitle') && this.localStorage.getItem('urunPrice')){
         productDetailTitle.innerText = this.localStorage.getItem('urunTitle')
+        productDetailPrice.innerText = this.localStorage.getItem('urunPrice')
     }
 })
 
--- Template Main/js/slider-test.js
+++ Template Main/js/slider-test.js
@@ -1,21 +1,73 @@
-<script>
-let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
+var urunArr = []
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+for (var x = 0; x < 20; x++) {
+	urunArr[x] = {
+		'title': 'Ürün İsmi',
+		'price': '1299 TL',
+		'detail': "Ürün detayı",
+		'category': "televizyon",
+		'seller': "satıcısı",
+		'src': "https://picsum.photos/200",
+	}
 }
 
-function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-   x[i].style.display = "none";  
-  }
-  x[slideIndex[no]-1].style.display = "block";  
+console.log("ürün array: ", urunArr)
+
+
+function addElement() {
+	const newDiv = document.createElement("div");
+
+	urunArr.forEach(element => {
+		newDiv.innerHTML += `
+		<div class="col-xl-3 col-lg-4 col-md-4 col-12">
+		<div class="single-product" onclick="seciliUrun(${element})">
+			<div class="product-img">
+				<a href="product-details.html">
+					<img class="default-img" src="${element.src}" alt="${element.title}">
+					<img class="hover-img" src="${element.src}" alt="${element.title}">
+				</a>
+				<div class="button-head">
+					<div class="product-action-2">
+						<a title="Sepete Ekle" href="#">Sepete Ekle</a>
+					</div>
+				</div>
+			</div>
+			<div class="product-content">
+				<h3><a href="product-details.html" class="urunTitle">${element.title}</a></h3>
+				<div class="product-price">
+					<span>${element.price}</span>
+				</div>
+			</div>
+	</div>
+	</div>`
+	});
+
+	document.getElementById('allUrunler').appendChild(newDiv)
+
 }
-</script>
+window.addEventListener('DOMContentLoaded', function () {
+	addElement()
+})
+/*
+		<div class="col-xl-3 col-lg-4 col-md-4 col-12">
+												<div class="single-product" onclick="seciliUrun(0)">
+													<div class="product-img">
+														<a href="product-details.html">
+															<img class="default-img" src="https://via.placeholder.com/550x750" alt="#">
+															<img class="hover-img" src="https://via.placeholder.com/550x750" alt="#">
+														</a>
+														<div class="button-head">
+															<div class="product-action-2">
+																<a title="Add to cart" href="#">Add to cart</a>
+															</div>
+														</div>
+													</div>
+													<div class="product-content">
+														<h3><a href="product-details.html" class="urunTitle">Polo Dress For Women </a></h3>
+														<div class="product-price">
+															<span>$29.00</span>
+														</div>
+													</div>
+												</div>
+											</div>
+*/

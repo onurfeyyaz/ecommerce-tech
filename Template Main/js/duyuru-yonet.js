173075ee726ebba53434a01fe3104fbaca951548
onurfeyyaz <feyyazonur0@gmail.com>
2022-12-04 21:22:58 +0300

    add duyuru detay

--- Template Main/js/duyuru-yonet.js
+++ Template Main/js/duyuru-yonet.js
@@ -1,16 +1,128 @@
-const duyuruSingle = document.querySelectorAll('.shop-single-blog')
+const detailArr = [
+    `Kampanya Koşulları:
+    • Kampanya 02.12.2022 08:00 – 05.11.2022 08:00 tarihleri arasında mediamarkt.com.tr veya MediaMarkt mobil uygulaması üzerinden seçili ürünlerde geçerlidir.
+    
+    • Dyson, Miele, Epson, Apple markalı ürünler, Xbox ve Sony PS5 modelleri, Tam Zamanı Kampanya ürünleri hariçtir.
+    
+    • Kampanyadan kazanabileceğiniz maksimum indirim tutarı 3.000 TL’dir.
+    
+    • 5.000-14.999 TL arası sepet tutarında indirim tutarı 300 TL, 15.000 TL-29.999 TL arası sepet tutarında indirim tutarı 1.000 TL, 30.000 TL ve üzeri sepet tutarında indirim tutarı 3.000 TL’dir.
+    
+    • Kampanya kapsamındaki hak nakit veya hediye kartı olarak talep edilemez.
+    
+    • Bu kampanya kapsamında verilen indirim ile diğer MediaMarkt CLUB avantajları ve Media Markt Turkey tarafından verilen diğer kampanyalar birleştirilemez.
+    
+    • MediaMarkt kampanya koşullarını değiştirme hakkını saklı tutmaktadır.
+    
+    • Kampanyadan bir müşteri yalnızca bir kere yararlanabilir. Aynı müşterinin kampanya kapsamında birden fazla ürün satın aldığı tespit edildiği takdirde ilk siparişten sonraki siparişler iptal edilecektir.
+    
+    • Kampanya ürünleri stoklarla sınırlıdır.`,
 
+    ` Eğitime özel Apple fiyatları ile yeni bir Mac veya iPad satın alırken indirimden yararlanın.
+    Üniversite öğrencileri, üniversiteye yeni kabul edilmiş öğrenciler, üniversite öğrencileri için ürün satın alan ebeveynler, öğretmenler ve tüm eğitim personeli için geçerlidir.Footnote*
+        
+    Ödemeyi taksitlere bölün.
+    3 aya kadar taksitle veya ödül puanlarıyla ödeyin.
+    Beş harika uygulama.
+    Güçlü bir koleksiyon.
+    Koşullara uygun üniversite öğrencileri, öğretmenler ve eğitim kuruluşları için Profesyonel Uygulama Paketi ile yaratıcı potansiyelinizi açığa çıkarın. Final Cut Pro, Motion, Compressor, Logic Pro ve MainStage. Video ve müzik için beş profesyonel uygulamadan oluşan bu koleksiyonun tamamını özel bir fiyata satın alın.
+
+    `,
+
+    `
+    1 
+    Satın almak istediğiniz yeni cihazın detay sayfasında bulunan “Eski Cihazını Yenile” kampanyasına tıklayın
+    2
+    Açılan sayfaya eski cihazınızın modelini, markasını ve diğer bilgilerini girin.
+    3
+    Eski cihazın geri alım bedelinin hesabınıza yatması için iletişim ve banka bilgilerinizi detaylıca girin.
+    4
+    Bu işlemlerden sonra yeni cihazınızı satın almak için sepet ekranına geri dönerek gerekli ödemeleri tamamlayın.
+    5
+    Eski cihazınızı, size en yakın mağazamıza teslim edin ya da belirttiğiniz adrese gönderilen “geri dönüş kiti” ile ücretsiz bir şekilde bize gönderin.
+    6
+    İncelemeler tamamlansın biz de eski cihazınızın geri adım bedelini hesabınıza yatıralım.
+    7
+    Tüm bu işlemleri (hiçbir kampanya farklılığı olmadan) mağazalarımızdan da gerçekleştirebilirsiniz.`,
+    ` 
+    Düğün Paketi
+Yeni bir hayata adım atarken çiftleri en çok kaygılandıran harcamaların başında eşyaların alınması geliyor. Uzayıp giden alışveriş listesini tamamlamak için mağaza mağaza dolaşmak, aile büyükleriyle birlikte eşyaları seçmek geleneği yavaş yavaş yerini online alışverişe bırakıyor. Özellikle büyük şehirlerdeki yoğun trafik ve ulaşım zorlukları evlilik hazırlığında olan çiftleri ve ailelerini alışverişi daha kolay ve pratik olarak yapma yolunu bulmaya itiyor. En kaliteli ve uygun fiyatlarla sunulan tüm markaların düğün paketleri, MediaMarkt güvencesiyle düğün alışverişini kolaylaştırıyor. Başta beyaz eşyalar olmak üzere yuvanız için yaptığınız listenin tamamını en uygun fiyatlarla sayfalarımızda bulabilirsiniz. Filtreleri kullanarak, full düğün paketleri beyaz eşyalı, beyaz eşya çeyiz paketi gibi çok sayıda seçeneği inceleyerek masa başında evinizi kurabilirsiniz. Şimdi bu ürünleri biraz daha yakından tanıyalım.
+
+Beyaz Eşyalı Düğün Paketlerinde Hangi Ürünler Yer Alır?
+Her evde bulunan ve gündelik işlerimizde bizlere büyük yardımları dokunan, evlerin kahramanları beyaz eşya paketi modelleri, içlerinde birden fazla çeşit bulundurarak birçok ihtiyacımızı karşılıyor. Gerek Türkiye menşeili gerekse dünya çapında birçok başarıya imza atmış popüler markalar, beyaz eşyaların üretim ve tasarımlarını yeni teknolojiye uyumlu olarak gerçekleştiriyor. Tüketicilere geniş yelpaze de birden fazla seçim yapma hakkı sunuluyor. Gün içerisinde bizlere büyük yardımları dokunan, hayatı kolaylaştıran beyaz eşyalardan bazılarını şu şekilde sıralayabiliriz:
+
+• Buzdolapları
+• Isıtıcılar
+• Bulaşık makineleri
+• Ocaklar
+• Çamaşır makineleri
+• Fırınlar
+• Derin dondurucular
+• Klimalar
+• Çamaşır kurutucuları
+• Davlumbazlar ve pişirme işlemleri esansında tüketicilere yardımı dokunan diğer teknolojik aletler
+Yukarıda sıraladığımız örnekler ya da örneklere benzer modeller arasına ek olarak evlerimizde yer alan ve elektronik ev aletleri başlığı altında geçen televizyon, radyo, müzik çalar ve benzeri teknolojik aletleri de sıralayabilirsiniz. Beyaz eşya setleri ile tüketicilere geniş yelpaze sunan markalar konusunda özellikle de yeni bir eve taşınma söz konusu olduğunda ürünlerin avantajları, dezavantajları ve tüketiciye sundukları hakkında araştırmalar yapabilirsiniz. Araştırmalar neticesinde özel tasarımlı teknolojik ya da beyaz ev eşya ürünlerinden başlayarak daha birçok farklı opsiyonlu özellikler ile donatılmış modellere kadar pek çok seçeneği göz önünde bulundurabilirsiniz.
+
+Teknolojinin her geçen gün bir önceki güne oranla yol kat etmesi ile birlikte birçok firma da ürünlerine yeni özellikler ekliyor ve piyasaya ürünlerini bu yeni özellikler ile sunarak model çeşitliliğini arttırıyor. Özelliklerden bazıları beyaz eşya ürünleri ya da teknolojik ürünlerin renkleri ve tasarımlarında yeniliğe doğru adım atarken, çoğu da bu ürünlerin tüketicilere sundukları fonksiyon ve özelliklerde görülebiliyor.
+
+Beyaz eşya sektöründeki gelişmeler ise genellikle kullanıcıların gün içerisinde duydukları ihtiyaçları karşılamaya yönelik tasarlanarak piyasaya sürülüyor ve set halinde bir bütün olarak tüketicilerin beğenisine sunuluyor. Beyaz eşyaların set halinde, bütün bir şekilde sunulmasının en büyük sebebi de tüketiciye olabildiğince avantajlı fiyatlara ihtiyaçlarını karşılamaya yönelik hizmetlerin verilmesinin istenmesi. Her evde ihtiyaç duyulabilecek beyaz eşyalara aynı paket içerisinde ve indirimli bir beyaz eşya seti ile ulaşmak tüketici için oldukça avantajlı bir kampanya olarak görülebiliyor.
+
+Beyaz eşya ürünlerinin boyutlarının büyük olması ve yerine getirmiş oldukları görev nedeniyle enerji tüketimlerinin fazla olması, tüketicilerin enerji tasarrufuna önem veren beyaz eşya ev aletlerine olan odağını arttırabiliyor. Özellikle beyaz eşya ev aletlerinin gün içerisinde sürekli açık tutulması ya da birden fazla kez kullanılması da enerji tasarrufu konusunun önemini büyük ölçüde arttırıyor.
+
+9 KG çamaşır makinesi ve bunun yanında A+++ enerji sınıfı çalışma özelliği gösteren modelleri beyaz eşya paketlerine verilebilecek örnekler arasında görebilirsiniz. Ayrıca dijital özellikler gösteren modeller de markaların sunmuş oldukları paketlerin içerisinde görebilirsiniz. Dokunmatik LCD ekrana sahip çamaşır makineleri, buzdolapları ya da akıllı telefonların yardımı ile kontrol edilebilen ve üstün opsiyonları bulunan diğer beyaz eşya modelleri de oldukça avantajlı.
+
+Beyaz eşya paketleri arasında yer alan ve en çok tercih edilen ürünlerden bir diğeri de gelişmiş opsiyonlara sahip bulaşık makineleridir. Yine çamaşır makinelerinde yer alan program sayılarının yoğunluğu da tüketiciler için ön plana çıkıyor. Beyaz eşya paketlerinde bulunan makinelerin şık ve dekoratif oluşları da önemli bir kriter olarak öne çıkabiliyor. Gerek beyaz eşyaların renkleri gerekse dış görünüşleri arasında beyaz eşya paketleri çeşitlenebiliyor. Paslanmaz çelik materyalleri, beyaz eşyaların ölçüleri ve ev içerisinde bulunan diğer ürünlerle uyum süreci de paketlerin tercih edilmesinde büyük önem taşıyor. En iyi ve en ucuz bulaşık makineleri ve düğün paketlerini MediaMarkt çatısı altında bulabilirsiniz. Karar kıldığınız ürünü ise gönül rahatlığıyla satın alabilirsiniz.
+
+Sizler de Türkiye'nin neresinde olduğunuz fark etmeksizin beyaz eşya paketi alışverişlerini MediaMarkt üzerinden, çeşitli fırsatları ve kampanyaları sayesinde kolayca tamamlayabilirsiniz. MediaMarkt tarafından evinize gönderilecek teknik servis ile de paket içerisinden çıkan beyaz eşyalarınızın kurulumlarını kolay ve pratik bir şekilde tamamlayabilir ve almış olduğunuz beyaz eşyalarınızı gönül rahatlığı ile garanti kapsamı içerisinde kullanabilirsiniz.`,
+
+    ` Değerli Müşterilerimiz,
+
+    Size özel büyük bir indirimle, oyuncu aksesuarlarınızı daha uygun fiyata alabileceğinizi sizlere duyurmaktan büyük bir mutluluk duyuyoruz.
+    
+    Şimdiye kadar almak istediğiniz oyuncu aksesuarlarına gerçekten yüksek fiyatlar ödemek zorunda kaldınız. Fakat artık herkesin ulaşabileceği fiyatlara oyuncu aksesuarlarını satın alma şansınız var.
+    
+    Bugünden itibaren, tüm müşterilerimizin kullanabileceği harika bir indirim kampanyamız var. Tüm oyuncu aksesuarlarında %50'ye varan çarpıcı indirimlerden yararlanabileceksiniz.
+    
+    Bu fırsatı kaçırmamalısınız! İndirimleri kaçırmamak için, oyuncu aksesuarlarınızı en kısa sürede edinin.
+    
+    GeForce ürünlerinde geçerli olan indirimler, 31 Aralık 2021 tarihine kadar geçerlidir.
+    
+    İndirimlerden faydalanmak ve oyuncu aksesuarlarınızı en uygun fiyata almak için, hemen alışverişe başlayın!
+    
+    Oyunculara özel indirimlerimizi kaçırmamak için, acele edin!
+    
+    Aksesuar özellikleri ile ilgili daha fazla bilgi almak için, lütfen bizimle iletişime geçin.
+    
+    Kulaklık ürünlerinde geçerli olan indirimleri kaçırmayın ve acele edin!`,
+
+    `Günlük işleriniz için güvenilir performans
+    Dizüstü bilgisayarınızın Intel® Core™ İşlemcisi ve geniş depolama alanı sayesinde günlük görevleri kolayca halledin.
+    Mikro kenarlı ekranıyla ince ve hafif
+Hafif tasarımıyla her yere taşıyabileceğiniz bu bilgisayarın titremesiz ve mikro kenarlı ekranı daha konforlu görüntüleme deneyimi sunar.
+Yeni bir bakış açısı edinin
+Windows 11, yeni bir deneyimle tutkularınızın peşinden gidebileceğiniz sakin ve yaratıcı bir alan sağlar. Yenilenmiş Başlat menüsünden, en sevdiğiniz kişilere, haberlere, oyunlara ve içeriklere bağlanmanın yeni yollarına kadar; Windows 11, doğal bir şekilde düşünmenin, ifade etmenin ve üretmenin yeridir.
+Full HD ekran
+Arkanıza yaslanın ve 2 milyon pikselin canlı kalitesiyle, kristal netliğinde görüntülerin keyfini çıkarın. 1920 x 1080 çözünürlük tüm dijital içeriklerinize yeni bir boyut kazandırır.
+yansıma önleyici panel
+yansıma önleyici panelle güneşin ve en sevdiğiniz içeriklerin keyfini çıkarın. Yansıma önleyici düşük parlaklığa sahip yüzey dışarıdayken daha az yansıma olmasını sağlar.
+Klavye tuşları tüketici sonrası geri dönüştürülmüş plastik içermektedir
+Tüketici sonrası geri dönüştürülmüş plastikten imal edilmiş tuşlarla klavyelerimizi daha sürdürülebilir tasarlıyoruz.
+HP QuickDrop
+Bilgisayar ve mobil cihazın arasında fotoğraf, video ve diğer belgeleri kablosuz olarak aktar.
+Adobe Teklifi
+HP'den bilgisayar satın alarak Adobe'un en iyi yaratıcılık ve üretkenlik uygulamalarının 1 aylık ücretsiz deneme sürümünü edinin. Hayal edebileceğiniz her şeyi üretin: fotoğraflar, videolar, grafikler, web sayfaları, PDF'ler ve daha fazlası. İhtiyaçlarınıza en uygun uygulama ile başlayın.`
+]
 function addDuyuruIcerikElement() {
     let newDiv = document.createElement("div");
 
-        newDiv.innerHTML = `
+    newDiv.innerHTML = `
         <div class="image" id="anaDuyuruImage">
-        <img src="${localStorage.getItem('duyuruImage')}" alt="#">
+        <img src="${localStorage.getItem('duyuruImg')}" alt="#">
     </div>
     <div class="blog-detail">
         <h2 class="blog-title">${localStorage.getItem('duyuruTitle')}</h2>
         <div class="blog-meta">
-            <span class="author"><a href="#"><i class="fa fa-user"></i>Admin</a><a href="#"><i class="fa fa-calendar"></i>${localStorage.getItem('duyuruTime')}</a></span>
+            <span class="author"><a href="#"><i class="fa fa-user"></i>Admin</a><a href="#"><i class="fa fa-calendar"></i>${localStorage.getItem('duyuruDate')}</a></span>
         </div>
         <div class="content">
             <p>${localStorage.getItem('duyuruDetail')}</p>
@@ -20,14 +132,25 @@ function addDuyuruIcerikElement() {
     document.getElementById('duyuruContent').append(newDiv)
 }
 
+const duyuruDate = document.querySelectorAll('.date')
+const duyuruImage = document.querySelectorAll('.duyuruImage')
+const duyuruTitle = document.querySelectorAll('.duyuruTitle')
+
+
+
 const seciliDuyuru = (data) => {
+
+    localStorage.removeItem('duyuruDate')
     localStorage.removeItem('duyuruTitle')
     localStorage.removeItem('duyuruImg')
     localStorage.removeItem('duyuruDetail')
 
-    localStorage.setItem('urunTitle',titleProduct[data].textContent)
-    localStorage.setItem('urunPrice',priceProduct[data].textContent.trim())
+    localStorage.setItem('duyuruDate', duyuruDate[data].textContent)
+    localStorage.setItem('duyuruTitle', duyuruTitle[data].textContent)
+    localStorage.setItem('duyuruImg', duyuruImage[data].src)
+    localStorage.setItem('duyuruDetail', detailArr[data])
 }
+
 window.addEventListener('DOMContentLoaded', function () {
     addDuyuruIcerikElement()
 })
